2f3f59cd1d78bc12085c55c2b42c0ef7611649ab
Prani Gopu <69959590+pranigopu@users.noreply.github.com>
2022-03-28 15:01:17 +0530

    Delete notes/chromeExtension/usingPopup directory

--- notes/chromeExtension/usingPopup/background.js
+++
@@ -1,7 +0,0 @@
-// To demonstrate that the onMessage event from the popup script is not detected by the background script...
-console.log("Background service worker, check.");
-
-chrome.runtime.onMessage.addListener(respondToMessage);
-function respondToMessage(message, sender, sendResponse){
-    console.log("Message received:", message)
-}
